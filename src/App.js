33cba3e70192edb2fc9a85c77fbb5e346f7347ae
Jillian <firedivine46@gmail.com>
2022-11-14 15:18:42 +1300

    made topbar, theme changer button inclueded

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-function app() {
-  return <div className="App"></div>
+import { colorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
+import Topbar from './scenes/global/Topbar'
+import DashBoard from './scenes/dashboard'
+
+function App() {
+  const [theme, colorMode] = useMode()
+
+  return (
+    <colorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<DashBoard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </colorModeContext.Provider>
+  )
 }
 
-export default app
+export default App
