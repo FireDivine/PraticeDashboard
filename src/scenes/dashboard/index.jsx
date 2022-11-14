f40351a0c02a614c783f4d3d14907a4c5d06e2bd
Jillian <firedivine46@gmail.com>
2022-11-14 16:52:37 +1300

    created header component

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,8 +1,15 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Header from '../../components/Header'
 
 const dashboard = () => {
   return (
-    <div>dashboard</div>
+    <Box m='20px' >
+      <Box display={'flex'} justifyContent='space-between' alignItems={'center'}>
+      <Header title="DASHBOARD" subtitle={"Welcome to your Dashboard"}/>
+      </Box>
+     
+    </Box>
   )
 }
 
